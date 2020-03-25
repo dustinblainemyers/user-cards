9b67b2cc70acddaad2505b6aa1f735010ec1cb82
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-25 14:35:48 -0400

    user-card component created

---
+++ src/components/user-card.jsx
@@ -0,0 +1,38 @@
+import React, { Component } from "react";
+
+class UserCard extends Component {
+  state = {
+    userData: 'Getting user card'
+  };
+    async componentDidMount() {
+       try {
+        const response = await fetch('https://randomuser.me/api/?results=1');
+        const data = await response.json();
+        console.log("data is ", data)
+        
+        // this.setState({
+        //   userData: data
+        // })
+        
+
+
+      } catch (error) {
+          this.setState({
+            userData: error.message
+          })
+
+      }
+        
+    }
+  render() {
+      const {UserCard} = this.state;
+    return (
+      <div>
+        <p>user data is ...</p>
+        <p> {userData}</p>
+      </div>
+    );
+  }
+}
+
+export default UserCard;
