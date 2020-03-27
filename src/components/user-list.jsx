1ecdb9dd7b3b02c1445bfdcb0f713e2a5b458c2c
Dustin Myers <dustinblaine.myers@gmail.com>
2020-03-27 12:33:34 -0400

    starting to refactor for multiple components

---
+++ src/components/user-list.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import UserCard from './components/user-card';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+
+      <UserCard/>
+    </div>
+  );
+}
+
+export default App;
